7a316991326912105de3da97675ceaa9b264b5e5
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2020-08-17 16:26:06 -0300

    Aula 4.2

--- exercicios/conversao/exercicio10.py
+++ exercicios/conversao/exercicio10.py
@@ -2,15 +2,34 @@
 # Faça um programa que peça o nome de 2 produtos, a quantidade comprada de cada produto (inteiro) e o valor (float) de cada um.
 # 
 # Mostre o nome a quantidade, preço por unidade e o total de cada produto.
+while True:
+    try:
+        # Variaveis produto 1
+        produto1_nome = input('Nome do produto: ')
+        produto1_quantidade = int(input('Quantidade: '))
+        produto1_valor = float(input('Preço: '))
+        #Variaveis produto 2
+        produto2_nome = input('Nome do produto: ')
+        produto2_quantidade = int(input('Quantidade: '))
+        produto2_valor = float(input('Preço: '))
+        #Calculo
+        produto2_total = float(produto2_valor*produto2_quantidade)
+        produto1_total = float(produto1_quantidade*produto1_valor)
+        #Exibir na tela
+        print(f'''
+Nome: {produto1_nome}
+Valor = R${produto1_valor:.2f}
+Quantidade = {produto1_quantidade}
+Total = R${produto1_total:.2f}
 
-def produtos:
-    nome = input('Nome: ')
-    preco = float(input('Valor: '))
-    quantidade = int(input('Quantidade: '))
-    def total(preco,quantidade):
-        return (float(preco) * float(quantidade))
-    def name(nome):
-        return nome
-
-produto1 = input('Nome: ')
-produto2 = input('Nome: ')
+Nome: {produto2_nome}
+Valor = R${produto2_valor:.2f}
+Quantidade = {produto2_quantidade}
+Total = R${produto2_total:.2f}
+''')
+    except ValueError:
+        print('Você digitou algo errado!')
+    else:
+        escolha = int(input('Digite 1 para reiniciar: '))
+        if escolha != 1:
+            break
--- exercicios/conversao/exercicio11.py
+++ exercicios/conversao/exercicio11.py
@@ -1,8 +1,19 @@
 # Exercicio 11
 # Escreva um programa que peça o peso (float) de 3 pessoas e motre na tela a soma de todos os pesos. 
-
-
-
-
-
-
+while True:
+    try:
+        #Variaveis
+        peso1 = float(input('Digite o peso 1 em Kg: '))
+        peso2 = float(input('Digite o peso 2 em Kg: '))
+        peso3 = float(input('Digite o peso 3 em Kg: '))
+        #Soma
+        soma = peso1 + peso2 + peso3
+        #Exibir
+        print(f'A soma de todos os pesos é igual a {soma:.3f}Kg')
+    except ValueError:
+        #Caso ocorra algum problema de digitação
+        print('Você digitou algo errado!')
+    else:
+        #Repetição ou saida
+        if int(input('Digite 1 para tentar novamente: ')) != 1:
+            break
