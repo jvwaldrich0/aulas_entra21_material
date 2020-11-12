ec15988fdb4a8bcadaa58773beef81b6ab7fdace
JOAO VITOR WALDRICH <joao.waldrich@alunos.sc.senac.br>
2020-11-12 15:44:52 -0300

    Rework

--- Mini-Projetos/Banco/Packages/funcoes.py
+++ Mini-Projetos/Banco/Packages/funcoes.py
@@ -1,8 +1,9 @@
+from os import name, system
+
+
 def verificacao(cliente):
-    if cliente.conta:
-        return 'Já possui conta'
-    else:
-        return 'Crie uma conta'
+    if not cliente.conta:
+        return print('Crie uma conta')
 
 
 def traco(tamanho: int):
@@ -12,3 +13,69 @@ def traco(tamanho: int):
 def listar(cliente):
     for i in range(len(cliente)):
         print(f'{i} - ' + str(cliente[i]))
+
+
+def clear():
+    if name == 'nt': # Windows
+        return system('cls') or None
+    else: # Linux / MacOS
+        return system('clear') or None
+
+
+def transferencia(cliente):
+    cliente[0]
+    indice = []
+    for x in range(2):
+        print(f'{"De quem" if x == 0 else "Para quem"}')
+        for i in range(len(cliente)):
+            print(f'{i} - ' + str(cliente[i]))
+        indice += [int(input('Digite o índice\n> '))]
+        verificacao(cliente[indice[x]])
+    else:
+        quantia = float(input('\nDigite a quantia\n> '))
+        if quantia > cliente[indice[0]].id_conta.saldo:
+            escolha = 0
+            while escolha != 1 or 2:
+                escolha = int(input('SALDO FICARÁ NEGATIVO! '
+                                    'DIGITE 1 PARA PROSSEGUIR'
+                                    'DIGITE 2 PARA CANCELAR OPERAÇÃO'))
+                if escolha == 1:
+                    cliente[indice[0]].id_conta.saldo -= quantia
+                    cliente[indice[1]].id_conta.saldo += quantia
+                elif escolha == 2:
+                    break
+                else:
+                    continue
+        else:
+            cliente[indice[0]].id_conta.saldo -= quantia
+            cliente[indice[1]].id_conta.saldo += quantia
+
+
+def deposito(cliente):
+    cliente[0]
+    listar(cliente)
+    indice = int(input('Digite o índice\n> '))
+    cliente[indice].id_conta.saldo += float(input('\nDigite a quantia\n> '))
+
+
+def view_saldo(cliente):
+    cliente[0]
+    listar(cliente)
+    print(traco(12))
+    indice = int(input('Digite o índice\n> '))
+    if cliente[indice].conta:
+        print(f'\n{traco(24)}\nR${cliente[indice].id_conta.saldo:.2f}\n{traco(24)}')
+
+
+def menu():
+    return f"""
+Digite a operação desejada
+{traco(24)}
+1 - Cadastrar Pessoa
+2 - Cadastrar Conta
+3 - Visualizar Saldo
+4 - Fazer uma transferência
+5 - Fazer um depósito
+6 - Sair
+{traco(24)}
+> """.upper()
--- Mini-Projetos/Banco/main.py
+++ Mini-Projetos/Banco/main.py
@@ -1,14 +1,16 @@
 from Packages.banco import Banco, Conta
 from Packages.pessoa import Pessoa
-from Packages.funcoes import verificacao, traco, listar
+from Packages.funcoes import verificacao, traco, listar, transferencia, deposito, view_saldo, menu
 from pickle import load, dump
 from os.path import join
 from os import getcwd as cwd
 from shutil import copyfile
 
 
+
 backup = False
 sem_arquivo = False
+# Abrir arquivo
 try:
     cliente = load(open('Data/data.txt', 'rb'))
     open('Data/Backup.txt', 'rb')
@@ -19,21 +21,11 @@ except FileNotFoundError:
     except FileNotFoundError:
         sem_arquivo = True
         cliente = []
-
 while True:
     try:
-        valor = int(input(
-            f"""
-Digite a operação desejada
-{traco(24)}
-1 - Cadastrar Pessoa
-2 - Cadastrar Conta
-3 - Visualizar Saldo
-4 - Fazer uma transferência
-5 - Fazer um depósito
-6 - Sair
-{traco(24)}
-> """.upper()))
+        # Menu
+        valor = int(input(menu()))
+        # Cadastrar Pessoa
         if valor == 1:
             cliente += [Pessoa(
                 nome=input(f'{traco(24)}\nDigite o nome: '),
@@ -42,8 +34,7 @@ Digite a operação desejada
                 mes=int(input('Mes = ')),
                 ano=int(input('Ano = ')),
             )]
-
-
+        # Cadastrar Conta
         elif valor == 2:
             cliente[0]
             listar(cliente)
@@ -57,51 +48,16 @@ Digite a operação desejada
                 )
                 cliente[indice].conta = True
             del indice
-
+        # Visualizar Saldo
         elif valor == 3:
-            cliente[0]
-            listar(cliente)
-            print(traco(12))
-            indice = int(input('Digite o índice\n> '))
-            if cliente[indice].conta:
-                print(f'\n{traco(24)}\nR${cliente[indice].id_conta.saldo:.2f}\n{traco(24)}')
-            del indice
-
+            view_saldo(cliente)
+        # Transferencia
         elif valor == 4:
-            cliente[0]
-            indice = []
-            for x in range(2):
-                print(f'{"De quem" if x == 0 else "Para quem" }')
-                for i in range(len(cliente)):
-                    print(f'{i} - ' + str(cliente[i]))
-                indice += [int(input('Digite o índice\n> '))]
-                print(verificacao(cliente[indice[x]]))
-            else:
-                quantia = float(input('\nDigite a quantia\n > '))
-                if quantia > cliente[indice[0]].id_conta.saldo:
-                    escolha = 0
-                    while escolha != 1 or 2:
-                        escolha = int(input('SALDO FICARÁ NEGATIVO! '
-                                            'DIGITE 1 PARA PROSSEGUIR'
-                                            'DIGITE 2 PARA CANCELAR OPERAÇÃO'))
-                        if escolha == 1:
-                            cliente[indice[0]].id_conta.saldo -= quantia
-                            cliente[indice[1]].id_conta.saldo += quantia
-                        elif escolha == 2:
-                            break
-                        else:
-                            continue
-                else:
-                    cliente[indice[0]].id_conta.saldo -= quantia
-                    cliente[indice[1]].id_conta.saldo += quantia
-            del indice
+            transferencia(cliente)
+        # Deposito
         elif valor == 5:
-            cliente[0]
-            listar(cliente)
-            indice = int(input('Digite o índice\n> '))
-            cliente[indice].id_conta.saldo += float(input('\nDigite a quantia\n> '))
-            del indice
-
+            deposito(cliente)
+        # Sair
         elif valor == 6:
             print("Agradecemos a sua visita!")
             break
@@ -110,9 +66,13 @@ Digite a operação desejada
     except IndexError:
         print(traco(24),'\nCadastre alguem...\n'.upper() + traco(24))
     else:
+        # Salvar arquivo
         dump(cliente, open('Data/data.txt', 'wb'))
-
+# recuperar Backup
 if backup:
-    copyfile(src=join(cwd(), 'Data/Backup.txt'), dst=join(cwd(), 'Data/data.txt'))
+    copyfile(src=join(cwd(), 'Data/Backup.txt'),
+             dst=join(cwd(), 'Data/data.txt'))
+# criar arquivo Backup
 elif sem_arquivo:
-    copyfile(src=join(cwd(), 'Data/data.txt'), dst=join(cwd(), 'Data/Backup.txt'))
+    copyfile(src=join(cwd(), 'Data/data.txt'),
+             dst=join(cwd(), 'Data/Backup.txt'))
